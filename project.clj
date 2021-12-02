4519cc9cd13aba9404b725af98753334302b1006
Dan Yankowsky <bytecrafter@gmail.com>
2021-12-02 11:16:45 -0500

    upgrade to Clojure 1.10.3

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
 (defproject adventofcode2021 "0.1.0-SNAPSHOT"
-  :dependencies [[org.clojure/clojure "1.10.1"]
+  :dependencies [[org.clojure/clojure "1.10.3"]
                  [medley "1.3.0"]]
   :repl-options {:init-ns adventofcode2021.day01})
