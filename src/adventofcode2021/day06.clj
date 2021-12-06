02dc12ee3e207f3ec2906395431ffd1cadc875cc
Dan Yankowsky <bytecrafter@gmail.com>
2021-12-06 00:33:31 -0500

    day06 code cleanup

--- src/adventofcode2021/day06.clj
+++ src/adventofcode2021/day06.clj
@@ -5,21 +5,7 @@
 (def input (map parse-int (str/split (read-input) #"[,\s]+")))
 ;(def input (map parse-int (str/split "3,4,3,1,2" #"[,\s]+")))
 
-(defn step-fish [fish]
-  (let [num-completed-fish (count (filter #{0} fish))
-        fish (concat (map
-                       (fn [f]
-                         (case f
-                           0 6
-                           (dec f)))
-                       fish)
-                     (repeat num-completed-fish 8))]
-    fish))
-
-(defn part1 []
-  (count (nth (iterate step-fish input) 80)))
-
-(defn step-fish-part2 [freqs]
+(defn step-fish [freqs]
   (let [num-spawns (get freqs 0 0)
         freqs (apply merge-with + (map (fn [[k v]]
                                          (let [k (if (= 0 k) 6 (dec k))]
@@ -28,9 +14,15 @@
         new-spawns {8 num-spawns}]
     (merge-with + freqs new-spawns)))
 
+(defn solve [days fish]
+  (let [freqs (frequencies fish)]
+    (reduce + 0 (vals (nth (iterate step-fish freqs) days)))))
+
+(defn part1 []
+  (solve 80 input))
+
 (defn part2 []
-  (let [freqs (frequencies input)]
-    (reduce + 0 (vals (nth (iterate step-fish-part2 freqs) 256)))))
+  (solve 256 input))
 
 (defn -main []
   (println (part1))
