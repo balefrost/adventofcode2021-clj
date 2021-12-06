547b59bd0627efa5ffc014b51ac448c858c6601e
Dan Yankowsky <bytecrafter@gmail.com>
2021-12-06 00:42:13 -0500

    day06 more code cleanup

--- src/adventofcode2021/day06.clj
+++ src/adventofcode2021/day06.clj
@@ -5,14 +5,18 @@
 (def input (map parse-int (str/split (read-input) #"[,\s]+")))
 ;(def input (map parse-int (str/split "3,4,3,1,2" #"[,\s]+")))
 
+(defn decrement-keys [m]
+  (into {}
+        (map (fn [[k v]] [(dec k) v]) m)))
+
 (defn step-fish [freqs]
-  (let [num-spawns (get freqs 0 0)
-        freqs (apply merge-with + (map (fn [[k v]]
-                                         (let [k (if (= 0 k) 6 (dec k))]
-                                           {k v}))
-                                       freqs))
-        new-spawns {8 num-spawns}]
-    (merge-with + freqs new-spawns)))
+  (let [reset-fish {6 (get freqs 0 0)}
+        dec-fish (dissoc
+                   (decrement-keys freqs)
+                   -1)
+        spawned-fish {8 (get freqs 0 0)}
+        freqs (merge-with + reset-fish dec-fish spawned-fish)]
+    freqs))
 
 (defn solve [days fish]
   (let [freqs (frequencies fish)]
