44311e8e1e0c592f50f8f5f5db3647c613d0d11e
Dan Yankowsky <bytecrafter@gmail.com>
2021-12-14 00:53:23 -0500

    day13 fun stuff

--- src/adventofcode2021/day13.clj
+++ src/adventofcode2021/day13.clj
@@ -18,6 +18,7 @@
      :folds folds}))
 
 (def input (process-input (read-input)))
+;(def input (process-input (read-input "day13_funny.txt")))
 ;(def input (process-input "6,10\n0,14\n9,10\n0,3\n10,4\n4,11\n6,0\n6,12\n4,1\n0,13\n10,12\n3,4\n3,0\n8,4\n1,10\n2,14\n8,10\n9,0\n\nfold along y=7\nfold along x=5"))
 
 (defn do-fold [dots fold]
@@ -48,7 +49,7 @@
           (str/join
             (for [x (range (inc xmax))]
               (if (contains? dots [x y])
-                "*"
+                "█"
                 " "))))))))
 
 (defn part2 []
