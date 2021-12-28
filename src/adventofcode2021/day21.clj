06b597a07d33593427a2f0259b6dc4e8f356db46
Dan Yankowsky <bytecrafter@gmail.com>
2021-12-28 00:26:24 -0500

    day21 part2

--- src/adventofcode2021/day21.clj
+++ src/adventofcode2021/day21.clj
@@ -14,8 +14,8 @@
     {:p1 p1
      :p2 p2}))
 
-;(def input (process-input (read-input)))
-(def input (process-input "Player 1 starting position: 4\nPlayer 2 starting position: 8"))
+(def input (process-input (read-input)))
+;(def input (process-input "Player 1 starting position: 4\nPlayer 2 starting position: 8"))
 
 (defn do-move [player spaces]
   (let [newpos (inc (rem (+ (dec (:pos player)) spaces) 10))
@@ -50,7 +50,73 @@
         (* (:total-rolls die) (:score (get players nextturn)))
         (recur die nextturn players)))))
 
-(defn part2 [])
+(def outcome-freqs
+  (into {}
+        (for [start (range 1 11)]
+          [start (frequencies
+                   (for [a (range 1 4)
+                         b (range 1 4)
+                         c (range 1 4)
+                         :let [sum (+ a b c)
+                               end (inc (rem (+ sum (dec start)) 10))]]
+                     end))])))
+
+(defn take-turn [score-freqs]
+  (reduce
+    #(merge-with + %1 %2)
+    {}
+    (for [[state state-freq] score-freqs
+          :let [{pos :pos score :score} state]
+          [new-pos move-freq] (get outcome-freqs pos)]
+      {{:pos new-pos :score (+ score new-pos)} (* state-freq move-freq)})))
+
+(defn compute-outcomes-seq [pos]
+  (letfn [(helper [freqs outcomes turn-no]
+            (lazy-seq
+              (cons
+                {:freqs freqs :outcomes outcomes :turn-no turn-no}
+                (if (not (empty? freqs))
+                  (let [new-freqs (take-turn freqs)
+                        turn-no (inc turn-no)
+                        win-states (filter
+                                     (fn [{score :score}]
+                                       (>= score 21))
+                                     (keys new-freqs))
+                        continue-states (filter
+                                          (fn [{score :score}]
+                                            (< score 21))
+                                          (keys new-freqs))
+                        win-freq (reduce + 0 (for [k win-states]
+                                               (get new-freqs k)))
+                        continue-freq (reduce + 0 (for [k continue-states]
+                                                    (get new-freqs k)))
+                        outcome {:wins      win-freq
+                                 :continues continue-freq}
+                        outcomes (assoc outcomes turn-no outcome)
+                        freqs (reduce dissoc new-freqs win-states)]
+                    (helper freqs outcomes turn-no))))))]
+    (helper
+      {{:pos pos :score 0} 1}
+      {0 {:wins 0 :continues 1}}
+      0)))
+
+(defn compute-outcomes [pos]
+  (:outcomes (last (compute-outcomes-seq pos))))
+
+(defn part2 []
+  (let [p1-outcomes (compute-outcomes (:p1 input))
+        p2-outcomes (compute-outcomes (:p2 input))
+        total-p1-wins (reduce + 0 (for [p1turns (keys p1-outcomes)
+                                        :let [p2turns (dec p1turns)
+                                              p1wins (get-in p1-outcomes [p1turns :wins])
+                                              p2continues (get-in p2-outcomes [p2turns :continues] 0)]]
+                                    (* p1wins p2continues)))
+        total-p2-wins (reduce + 0 (for [p2turns (keys p2-outcomes)
+                                        :let [p1turns p2turns
+                                              p2wins (get-in p2-outcomes [p2turns :wins])
+                                              p1continues (get-in p1-outcomes [p1turns :continues] 0)]]
+                                    (* p2wins p1continues)))]
+    (max total-p1-wins total-p2-wins)))
 
 (defn -main []
   (try
